8a01f736f34769566a1c01e9aa12e445bf49f9e1
Madhav-Murali-NITC <madhav_b210027cs@nitc.ac.in>
2024-02-12 01:14:37 +0530

    added contacts page+animations

--- src/pages/Hero.js
+++ src/pages/Hero.js
@@ -1,20 +1,35 @@
 import styles from "./Desktop.module.css";
 import Spline from "@splinetool/react-spline";
+import React from "react";
+import {useState,useEffect} from "react";
 
 const Desktop = () => {
+  
+  const [isPlaying, setIsPlaying] = useState(false);
+
+  const togglePlay = () => {
+    console.log(isPlaying)
+    setIsPlaying(!isPlaying);
+  };
+
+
+
+
+
   return (
+    <div className = 'container'>
     <div className={styles.desktop1}>
-      <audio src = 'Lucki.mp3' autoPlay loop />
+    <audio src = 'Lucki.mp3' control = {isPlaying} autoPlay loop/>
       <img
         className={styles.v602Nunoon33Rippednotes1Icon}
         alt=""
         src="/v602nunoon33rippednotes-1@2x.png"
       />
-      <div className={styles.rectangleIcon}>
+      <div  onClick = {togglePlay}  className={styles.rectangleIcon}>
       <Spline scene="https://prod.spline.design/7d62JuMMdrD0x-QZ/scene.splinecode" />
       </div>
       
-      <div className={styles.visual}>Visual</div>
+      <div  className={styles.visual}>Visual</div>
       <div className={styles.audio}>Audio</div>
       <img className={styles.desktop1Child} alt="" src="/vector-1.svg" />
       <img className={styles.desktop1Item} alt="" src="/vector-2.svg" />
@@ -22,7 +37,7 @@ const Desktop = () => {
       <div className={styles.rectangleDiv} />
       <img className={styles.image1Icon} alt="" src="/image-1@2x.png" />
       <div className={styles.div}>*</div>
-      <a href="https://www.youtube.com/"><div className={styles.contact}>contact</div></a>
+      <a href="https://www.youtube.com/"><div className={styles.contact}><p>contact</p></div></a>
       <div className={styles.text}>{` `}</div>
       <div className={styles.text1}>{` `}</div>
       <div className={styles.rectangleParent}>
@@ -30,6 +45,7 @@ const Desktop = () => {
         <div className={styles.groupItem} />
         <div className={styles.groupInner} />
       </div>
+    </div>
     </div>
   );
 };
